5344f19ca2c4d3cede931f677aaab4ba91110d8e
MuhammadTalha-2 <talha.inshalytics@gmail.com>
2026-05-21 16:31:37 +0500

    first commit

---
+++ app/routes/app._index.jsx
@@ -0,0 +1,23 @@
+import { boundary } from "@shopify/shopify-app-react-router/server";
+import { authenticate } from "../shopify.server";
+
+export const loader = async ({ request }) => {
+  await authenticate.admin(request);
+  return null;
+};
+
+export default function Index() {
+  return (
+    <s-page heading="SubsExport">
+      <s-section heading="Dashboard">
+        <s-paragraph>
+          Connect your subscription apps and export subscriber data.
+        </s-paragraph>
+      </s-section>
+    </s-page>
+  );
+}
+
+export const headers = (headersArgs) => {
+  return boundary.headers(headersArgs);
+};
